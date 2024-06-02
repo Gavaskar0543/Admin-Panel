4423fce8ddb12ccdc94a15c78abaaa7c9be886de
gavaskaraeg <gavaskaraeg@gmail.com>
2024-06-02 05:09:06 +0000

    navbar done

--- Frontend/admin_panel/src/Components/Dashboard/Navbar.jsx
+++ Frontend/admin_panel/src/Components/Dashboard/Navbar.jsx
@@ -1,11 +1,11 @@
 const Navbar = ()=>{
     return(
-        <div className="navbar flex justify-between items-center ">
+        <div className="navbar flex justify-between items-center py-2 px-2 ">
            <div>
-          <img src="/assets/navbar.png" className="logo"  alt="logo"/>
+          <img src="/assets/navbar.png" className="" width={100}  alt="logo"/>
            </div>
            <div>
-           <img src="/assets/user.png" className="userimg" alt="user"/>
+            <img src="/assets/user.png" width={25} alt="user"/>
            </div>
         </div>
     )
