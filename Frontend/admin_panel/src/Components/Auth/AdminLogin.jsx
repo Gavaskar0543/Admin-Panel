72984ffdc6ffe0f81be1379af5e787cf737a9156
gavaskaraeg <gavaskaraeg@gmail.com>
2024-06-03 06:54:19 +0000

    form ready

--- Frontend/admin_panel/src/Components/Auth/AdminLogin.jsx
+++ Frontend/admin_panel/src/Components/Auth/AdminLogin.jsx
@@ -3,7 +3,7 @@ const AdminLogin =() =>{
      <div className="auth-container ">
         <div className="bg-white border auth-form rounded-lg">
             <form>
-                <div className="w-full flex flex-col mt-4 justify-center items-center border">
+                <div className="w-full flex flex-col mt-4 justify-center items-center ">
                 <div className="mt-3 mb-4 flex flex-col mb-3">
                     <div className="w-full flex justify-center items-center">
                     <img src="/assets/homeImg.png" width={130} alt="logo" />
@@ -14,16 +14,16 @@ const AdminLogin =() =>{
                 </div>
                 <div>
                     <div className="mb-3">
-                        <label for="email">Email-id</label><br/>
-                        <input type="email" id="email" className="w-64 px-2 py-2 border border-slate-300 focus:outline-none rounded-lg"/>
+                        <label for="email " className="text-md font-thin text-slate-600">Email-id</label><br/>
+                        <input type="email" id="email" className="w-64 px-2 py-2  border border-slate-300 focus:outline-none rounded-lg"/>
                         </div>
                     <div className="mb-3">
-                        <label for="password" className="text-sm font-semibold text-slate-600">Password</label><br/>
+                        <label for="password" className=" text-md font-thin text-slate-600">Password</label><br/>
                         <input type="password" id="password" className="w-64 px-2 border border-slate-300 focus:outline-none py-2 rounded-lg"/>
 
                     </div>
                     <div>
-                        <p className="text-sm text-end font-semibold">Forgot Password?</p>
+                        <p className="text-sm forgot-password text-end font-semibold">Forgot Password?</p>
                     </div>
 
                 </div>
